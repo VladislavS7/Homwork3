45be839167fab8dc66d1d0e4f24da68e402ec4f3
Susidko Vladislav <vladislavsus7@gmail.com>
2022-04-09 22:10:16 +0300

    new class StudentCard

---
+++ Homework3/StudentCard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework3
+{
+    public class StudentCard
+    {
+        public int Count { get; set; }
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
